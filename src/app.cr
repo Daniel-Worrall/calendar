d089b6dae988d771d7fb6477e4603e3d905c3c36
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2020-02-10 15:46:10 +0000

    Personalisation

--- src/app.cr
+++ src/app.cr
@@ -2,18 +2,14 @@ require "./calendar"
 require "kemal"
 require "./geolite/geolite"
 
-geloite = Geolite.new("GeoLite2-City.mmdb")
+timezone = Time::Location.load("America/Denver")
 
-client = Calendar::Client.new(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], ENV["ACCESS_TOKEN"], ENV["EXPIRES_IN"], ENV["REFRESH_TOKEN"], ENV["SCOPE"])
+client = Calendar::Client.new(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], ENV["ACCESS_TOKEN"], ENV["EXPIRES_IN"], ENV["REFRESH_TOKEN"], ENV["SCOPE"], timezone)
 id = ENV["CALENDAR_ID"].not_nil!
 
 get "/" do |env|
   env.response.content_type = "application/rss+xml"
-  timezone = Time::Location::UTC
-  if ip = env.request.remote_address.try &.split(":").first
-    timezone = geloite.timezone(ip)
-  end
-  client.rss(id, timezone)
+  client.rss(id)
 end
 
-Kemal.run
+Kemal.run(ENV["CALENDAR_PORT"].to_i)
--- src/calendar/client.cr
+++ src/calendar/client.cr
@@ -6,47 +6,29 @@ module Calendar
     getter calendars = {} of String => Array(Google::Event)
     getter cache = {} of String => Time::Span
 
-    def initialize(client_id : String, client_secret : String, access_token : String, expires_at : String, refresh_token : String, scope : String)
+    def initialize(client_id : String, client_secret : String, access_token : String, expires_at : String, refresh_token : String, scope : String, @timezone : Time::Location)
       @googleauth = Google::Auth::TokenStore.new(client_id, client_secret, access_token, expires_at, refresh_token, scope)
     end
 
-    def rss(calendar_id : String, timezone)
+    def rss(calendar_id : String)
       events = cache(calendar_id)
-      time = Time.utc
+      time = Time.local(@timezone)
 
       XML.build do |xml|
         xml.element("rss") do
           xml.element("channel") do
             xml.element("title") { xml.cdata "Calendar Events" }
             xml.element("description") { xml.cdata "Events Synced from Google Calendar" }
-            xml.element("ttl") { xml.text "1" }
             events.each do |event|
               xml.element("item") do
-                start = event.start.in(timezone)
-                span = start - time
+                start = event.start.in(@timezone)
                 title = String.build do |string|
-                  case time
-                  when .< start
-                    string << span.days
-                    string << "d "
-                    string << span.hours.to_s.rjust(2, '0')
-                    string << "h "
-                    string << span.minutes.to_s.rjust(2, '0')
-                    string << "m "
-                  when .< event.end
-                    string << "ONGOING - "
-                  else
-                    string << "ENDED - "
-                  end
                   string << event.summary
-                end
-                xml.element("title") { xml.cdata title }
-                description = String.build do |string|
+                  string << " "
+                  start.to_s("%R ", string)
                   duration = event.end - start
-                  start.to_s("%F %R ", string)
-                  string << start.zone.name
                   if duration >= Time::Span.new(0, 1, 0)
-                    string << " ("
+                    string << "("
                     if duration.days > 0
                       string << duration.days
                       string << "d "
@@ -63,8 +45,9 @@ module Calendar
                     string << ")"
                   end
                 end
-                xml.element("description") { xml.cdata description }
-                xml.element("link") { xml.text "test" }
+                xml.element("title") { xml.cdata title }
+                xml.element("link") { xml.text time > start ? "https://twitch.tv/koibu" : "https://a.chronus.eu/#{(start.to_unix // 60).to_s(16)}" }
+                xml.element("pubdate") { xml.text start.to_rfc2822 }
               end
             end
           end
@@ -83,7 +66,7 @@ module Calendar
     end
 
     private def events_from_calendar(calendar_id : String)
-      Google.get_calendar_list(@googleauth.token, calendar_id, Time.utc.at_beginning_of_day, (Time.utc + Time::Span.new(14, 0, 0, 0)))
+      Google.get_calendar_list(@googleauth.token, calendar_id, Time.local(@timezone).at_beginning_of_day, (Time.utc + Time::Span.new(14, 0, 0, 0)))
     end
 
     private def valid_cache?(calendar_id)
